b8c1151e2351dbd0d11fd48304dc75e722a9d698
Tri Nanda <zidanecr7kaka@gmail.com>
2019-08-22 15:03:54 +0700

    jinja template inheritance for teacher and student templates

--- app/users/operator/views.py
+++ app/users/operator/views.py
@@ -15,14 +15,24 @@ def index():
 
 @operator.route('/operator_dashboard')
 def operator_dashboard():
-    return render_template('main/operator/index.html')
+    return render_template('main/operator/operator-dashboard.html')
 
 
 @operator.route('/all_teachers')
 def all_teachers():
-    return render_template('main/operator/all_teachers.html')
+    return render_template('main/operator/all-teachers.html')
 
 
 @operator.route('/teacher_profile')
 def teacher_profile():
-    return render_template('main/operator/teacher_profile.html')
+    return render_template('main/operator/teacher-profile.html')
+
+
+@operator.route('/all_students')
+def all_students():
+    return render_template('main/operator/all-students.html')
+
+
+@operator.route('/student_profile')
+def student_profile():
+    return render_template('main/operator/student-profile.html')
