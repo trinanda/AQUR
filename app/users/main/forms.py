8edc573e3789ab9fd3bc32433ab886b9937eacba
Tri Nanda <zidanecr7kaka@gmail.com>
2019-12-15 17:16:01 +0700

    one step form for new students

---
+++ app/users/main/forms.py
@@ -0,0 +1,41 @@
+from flask_wtf import FlaskForm
+from wtforms import FieldList, FormField
+from wtforms.ext.sqlalchemy.fields import QuerySelectField
+from wtforms.validators import required
+from wtforms.fields import StringField, SubmitField, SelectField
+from wtforms import ValidationError, TextAreaField
+from wtforms.fields.html5 import EmailField, DateField, TimeField
+from wtforms.validators import InputRequired, Length, Email, DataRequired
+from flask_babel import _, lazy_gettext as _l
+from app.models import User, gender, Course, type_of_class, day_name_list
+
+
+class TimeScheduleForm(FlaskForm):
+    day = SelectField(_l('Day'), validators=[required()], choices=day_name_list)
+    time = TimeField(_l('Time'), validators=[required()])
+
+
+class OneStepForm(FlaskForm):
+    first_name = StringField(_l('First name'), validators=[InputRequired(), Length(1, 64)])
+    last_name = StringField(_l('Last name'), validators=[InputRequired(), Length(1, 64)])
+    date_of_birth = DateField(_l('Date of birth'), validators=[DataRequired()], format='%Y-%m-%d')
+    address = StringField(_l('Address'), validators=[InputRequired(), Length(1, 255)])
+    gender = SelectField(_l('Gender'), choices=gender)
+    email = EmailField(_l('Email'), validators=[InputRequired(), Length(1, 64), Email()])
+    phone_number = StringField(_l('Phone number, e.g: 081234567890'), validators=[InputRequired(), Length(1, 12)])
+    course_name = QuerySelectField(_l('Course name'),
+                                   validators=[required('It seems the student didn\' pay any course ')],
+                                   query_factory=lambda: Course.query.all())
+    type_of_class = SelectField(_l('Type of class'), validators=[required()], choices=type_of_class)
+    time_schedule = FieldList(FormField(TimeScheduleForm, label=''), min_entries=2, label='')
+    description = TextAreaField(_l('Reason'), validators=[required()])
+    submit = SubmitField()
+
+    def validate_phone_number(self, field):
+        if User.query.filter_by(phone_number=field.data).first():
+            raise ValidationError(_('Duplicate phone number with the other users in this system, '
+                                    'please input different phone number!'))
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError(_('Email already registered.'))
--- app/users/main/views.py
+++ app/users/main/views.py
@@ -1,7 +1,11 @@
-from flask import Blueprint, render_template, url_for
+from flask import Blueprint, render_template, url_for, flash
 from werkzeug.utils import redirect
 
-from app.models import EditableHTML
+from app.models import EditableHTML, Role, Student, User, RequisitionSchedule, TimeSchedule, Course, \
+    RequisitionScheduleStatus
+from flask_babel import _
+from app import db
+from app.users.main.forms import OneStepForm
 
 main = Blueprint('main', __name__)
 
@@ -19,3 +23,68 @@ def about():
     editable_html_obj = EditableHTML.get_editable_html('about')
     return render_template(
         'main/about.html', editable_html_obj=editable_html_obj)
+
+
+@main.route('/form', methods=['POST', 'GET'])
+def one_step_form():
+    set_defatul_student_role = Role.query.filter_by(index='student').first()
+    form = OneStepForm()
+    if form.validate_on_submit():
+        first_name = str(form.first_name.data)
+        last_name = str(form.last_name.data)
+
+        # TODO | InsyaAllah will working in the line bellow | what if user doesn't have email..?
+        email = form.email.data
+        if email is None:
+            number = 0
+            while True:
+                number += 1
+                if User.query.filter_by(email=email).first is not None:
+                    email = "".join(first_name.split()) + "".join(last_name.split()) + str(number) + '@gmail.com'
+                else:
+                    return False
+
+        student = Student(
+            role=set_defatul_student_role,
+            first_name=first_name,
+            last_name=last_name,
+            gender=form.gender.data,
+            date_of_birth=form.date_of_birth.data,
+            address=form.address.data,
+            email=email,
+            phone_number=form.phone_number.data,
+            password='1',  # TODO | set all password to '1' before the student login feature available
+            confirmed=True,
+        )
+        db.session.add(student)
+
+        list_of_dict_time_schedule = []
+        for entry in form.time_schedule:
+            list_of_dict_time_schedule.append(
+                {'day': entry.data['day'], 'time': entry.data['time'].strftime('%H:%M')})
+
+        course = Course.query.filter_by(name=str(form.course_name.data)).first()
+
+        requisition_schedule = RequisitionSchedule(
+            student_id=student.id,
+            course_id=course.id,
+            type_of_class=form.type_of_class.data,
+            how_many_times_in_a_week=2,
+            requisition_status=RequisitionScheduleStatus.STUDENT_REQUISITION.name,
+            note=form.description.data,
+        )
+
+        for data in list_of_dict_time_schedule:
+            time_schedule = TimeSchedule(day=data['day'], start_at=data['time'])
+            requisition_schedule.time_schedule.append(time_schedule)
+
+        db.session.add(requisition_schedule)
+        try:
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            flash(str(e), 'error')
+            return redirect(url_for('main.one_step_form'))
+        flash(_('Successfully added new data'), 'success')
+        return redirect(url_for('main.one_step_form'))
+    return render_template('main/one-step-form.html', form=form)
